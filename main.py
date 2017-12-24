258742ec7fe3bc985a96d14792ea9116b1641eaa
info@rangeanalyticsusa.com <datastorage2017>
2017-12-24 16:05:10 +0500

    fourth commit

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+	
+@app.route('/fawad')
+def hello_world():
+    return 'Hello fawad!'
 
 if __name__ == '__main__':
     app.run()
